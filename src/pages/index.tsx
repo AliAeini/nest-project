f8800c9a32e4a9a1f12b688e0e66d7372e217bde
AliAeini <aliaeini123@gmail.com>
2024-02-25 14:27:48 +0330

    create banner section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
-import {Section} from "@/components";
+import {Banner, Section} from "@/components";
 
 export default function Home() {
   return (
       <Section>
-       <></>
+            <Banner title={"Don’t miss amazing grocery deals"}
+                    subTitle={"Sign up for the daily newsletter"}
+                    image={"/assets/images/fresh-apples.png"}
+                    bg_image={"/assets/images/banner_bg.png"}
+            />
       </Section>
   );
 }
